d8f2b051bb960a34b84bcc10a711ad5570776c70
Francesco <74672573+fcipollone-ledger@users.noreply.github.com>
2021-05-27 10:13:18 +0200

    Add files via upload

---
+++ dp_dev.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Author : Francesco
+# Copyright (c) 
+# Script :
+
+cat _config_main_edit.yml _config_dev.yml > _config.yml
+bundle install
+bundle exec jekyll build
+bundle exec jekyll serve --incremental --drafts --watch --future
