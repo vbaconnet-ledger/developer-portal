e0646a01255a6575d0fb6f839709c8d9028de88e
fcipollone-ledger <74672573+fcipollone-ledger@users.noreply.github.com>
2021-06-17 16:50:26 +0200

    Simplified _Config.yml

--- dp_dev.sh
+++ dp_dev.sh
@@ -4,7 +4,7 @@
 # Copyright (c) 
 # Script :
 
-cat _config_main_edit.yml _config_dev.yml > _config.yml
+# cat _config_main_edit.yml _config_dev.yml > _config.yml
 bundle install
 bundle exec jekyll build
 bundle exec jekyll serve --incremental --drafts --watch --future
